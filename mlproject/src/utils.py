203f892995bebcf1812f08677ebf5f73d4b5954c
vivekvardhan1512 <vivekvardhan1512@gmail.com>
2025-10-23 12:27:22 +0530

    Hyper Parameter Tuning

--- mlproject/src/utils.py
+++ mlproject/src/utils.py
@@ -5,11 +5,12 @@ from mlproject.src.exception import CustomException
 from mlproject.src.logger import logging
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV, cross_val_score
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok=True)  # ✅ Correct usage
+        os.makedirs(dir_path, exist_ok=True)  
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
@@ -20,13 +21,19 @@ def save_object(file_path, obj):
         logging.error(f"Error while saving object: {e}")
         raise CustomException(e, sys)
 
-def evaluate_models(X_train,y_train,X_test, y_test,models):
+def evaluate_models(X_train,y_train,X_test, y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
 
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            logging.info(f"Best parameters for model {list(models.keys())[i]}: {gs.best_params_}")
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
